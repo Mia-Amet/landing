fed545668c15abe28616493d91cf813535c9326d
dashaHsh <dashaags@gmail.com>
2019-05-27 23:11:43 +0300

    small bugs fixed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="position-absolute" id="overlay"></div>
     <video autoplay loop src="https://videos.ctfassets.net/oqn5qhpsy9i3/4LypTYVmrxLn9Vb9STV3Dk/25ca5ef44ed32fdf87fd8670b160eb03/desktop4.mp4" 
-           id="video-bg" class="position-relative" preload></video>
+           id="video-bg" class="position-relative" preload videoSize></video>
   `,
   styleUrls: ['./header.component.scss']
 })
